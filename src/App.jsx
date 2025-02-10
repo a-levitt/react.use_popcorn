45e6946ab4a98080b230748b699c80959477e3f3
Artem Trifonov <artemis.levitt@gmail.com>
2025-02-10 21:09:51 +0100

    Make reusable Box component with composition

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import NavBar from './components/Navbar';
 import Main from './components/Main';
 import {useState} from "react";
 import {tempMovieData} from "./MoviesData.js";
+import {tempWatchedData} from "./MoviesData.js";
+import WatchedSummary from "./components/WatchedSummary.jsx";
+import WatchedList from "./components/WatchedList.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import NumResults from "./components/NumResults.jsx";
-import ListBox from "./components/ListBox.jsx";
-import WatchedBox from "./components/WatchedBox.jsx";
+import Box from "./components/Box.jsx";
 import MoviesList from "./components/MoviesList.jsx";
 
 export default function App() {
     const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
 
     return (
         <>
@@ -18,12 +21,15 @@ export default function App() {
                 <SearchBar />
                 <NumResults movies={movies} />
             </NavBar>
-            
+
             <Main>
-                <ListBox>
+                <Box>
                     <MoviesList movies={movies} />
-                </ListBox>
-                <WatchedBox />
+                </Box>
+                <Box>
+                        <WatchedSummary watched={watched} />
+                        <WatchedList watched={watched} />
+                </Box>
             </Main>
         </>
     );
