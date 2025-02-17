d1ea8bf52ec7bc22d6ea86f5cf743717e2488086
Artem Trifonov <artemis.levitt@gmail.com>
2025-02-17 20:03:01 +0100

    Use PropTypes for StarRating values types

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {PropTypes} from 'prop-types';
 
 const containerStyle = {
     display: "flex",
@@ -9,6 +10,17 @@ const containerStyle = {
 const starContainerStyle = {
     display: "flex",
 };
+
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    /*maxRating: PropTypes.number.isRequired,*/
+    defaultRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    messages: PropTypes.array,
+    className: PropTypes.string,
+    onSetRating: PropTypes.func,
+}
 
 export default function StarRating({maxRating = 5,
                                        color = "#fcc419",
