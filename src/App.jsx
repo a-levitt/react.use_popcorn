78bb33cd7143132cc29a1ecc20a3ea057fa3575a
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-07 10:29:42 +0100

    Create default hook useMovies

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,14 @@ import NumResults from "./components/NumResults.jsx";
 import Box from "./components/Box.jsx";
 import MoviesList from "./components/MoviesList.jsx";
 import Loader from "./components/Loader.jsx";
-import {KEY} from "./secured/APIKey.js";
 import ErrorMessage from "./components/ErrorMessage.jsx";
 import MovieDetails from "./components/MovieDetails.jsx";
+import {useMovies} from "./hooks/useMovies.jsx";
 
 export default function App() {
     const [query, setQuery] = useState("Who");
-    const [movies, setMovies] = useState([])/*(tempMovieData)*/;
-    const [isLoading, setIsLoading] = useState(false);
-    const [err, setErr] = useState("");
     const [selectedId, setSelectedId] = useState(null);
+    const {movies, isLoading, err} = useMovies(query);
 
     // const [watched, setWatched] = useState([])/*(tempWatchedData)*/;
     const [watched, setWatched] = useState(() => {
@@ -49,48 +47,6 @@ export default function App() {
     useEffect(() => {
         localStorage.setItem("watched", JSON.stringify(watched));
     }, [watched])
-
-    useEffect(function ()  {
-        const controller = new AbortController();
-
-        async function fetchMovies() {
-            try {
-                setIsLoading(true);
-                setErr("");
-                const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-                    {signal: controller.signal});
-
-                if (!res.ok) throw new Error(res.statusText);
-
-                const data = await res.json();
-
-                if (data.Response === "False") throw new Error("Movie not found");
-
-                setMovies(data.Search);
-                setErr("");
-            } catch (error) {
-                console.log(error.message);
-
-                if (error.name !== "AbortError") {
-                    setErr(error.message);
-                }
-            } finally {
-                setIsLoading(false);
-            }
-        }
-
-        if (query.length < 3) {
-            setMovies([]);
-            setErr("");
-            return
-        }
-
-        fetchMovies();
-
-        return function () {
-            controller.abort();
-        }
-    }, [query])
 
     return (
         <>
---
+++ src/hooks/useMovies.jsx
@@ -0,0 +1,54 @@
+import {useEffect, useState} from "react";
+import {KEY} from "../secured/APIKey.js";
+
+function useMovies(query) {
+    const [movies, setMovies] = useState([])/*(tempMovieData)*/;
+    const [isLoading, setIsLoading] = useState(false);
+    const [err, setErr] = useState("");
+
+    useEffect(function ()  {
+        const controller = new AbortController();
+
+        async function fetchMovies() {
+            try {
+                setIsLoading(true);
+                setErr("");
+                const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                    {signal: controller.signal});
+
+                if (!res.ok) throw new Error(res.statusText);
+
+                const data = await res.json();
+
+                if (data.Response === "False") throw new Error("Movie not found");
+
+                setMovies(data.Search);
+                setErr("");
+            } catch (error) {
+                console.log(error.message);
+
+                if (error.name !== "AbortError") {
+                    setErr(error.message);
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        if (query.length < 3) {
+            setMovies([]);
+            setErr("");
+            return
+        }
+
+        fetchMovies();
+
+        return function () {
+            controller.abort();
+        }
+    }, [query]);
+
+    return {movies, isLoading, err};
+}
+
+export { useMovies };
